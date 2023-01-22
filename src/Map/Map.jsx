1352c0495788d8980f5894b2a6d40c2a02b2358e
amithap <amithap@embla.asia>
2023-01-22 16:16:21 +0530

    fix lint errors

--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import { Rating } from '@material-ui/lab';
-
+// import { useMediaQuery } from '@material-ui/core';
 import useStyles from './styles';
 
 const Map = () => {
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
+    // const isMobile = useMediaQuery('(min-width:600px)');
 
     const cordinates = { lat: 0, lng: 0 }
 
